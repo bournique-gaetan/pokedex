55bdcc6bdc8d3c834e0bbfdd883459f2536f77de
bournique gaetan <bourniquegaetan@gmail.comgit config --global pull.rebase falsejsls>
2023-03-28 14:08:36 +0200

    quetes 3 react

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,31 @@
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-            <figcaption>bulbasaur</figcaption>
-        </figure>
+        pokemon.imgSrc !== undefined ?
+            (
+                <figure>
+                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                    <figcaption>{pokemon.name}</figcaption>
+                </figure>
+            ) : (
+                <figure>
+                    <p>???</p>
+                    <figcaption>{pokemon.name}</figcaption>
+                </figure>
+            )
+
     )
 }
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 export default PokemonCard;
