fa54d69839df530b6e5261fea789f2bffa4915be
bournique gaetan <bourniquegaetan@gmail.comgit config --global pull.rebase falsejsls>
2023-03-28 16:42:28 +0200

    props quests 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,25 @@ import MyTitle from './components/MyTitle'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
-
-
   return (
     <div className="App">
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
 
     </div>
   )
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
 
 export default App
